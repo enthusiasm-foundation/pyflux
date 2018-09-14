23db085a223f5120837f149b9bae5a3d4872129d
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2018-09-14 19:44:08 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup
+
+setup_args = dict (
+    name="pyflux",
+    version="0.9",
+    description= 'A Python Client for fluxlang',
+    license='MIT',
+    url='https://github.com/enthusiasm-foundation/pyflux',
+    author='PyFlux Mafia',
+    author_email='gianarb@influxdata.com',
+    packages=[ "pyflux" ],
+    install_requires = [ 'setuptools',
+                         'pandas',
+                         'json2html',
+                         'requests',
+                         'matplotlib',
+                         'pygments'
+    ])
+
+
+if __name__ == '__main__':
+    setup( **setup_args )
